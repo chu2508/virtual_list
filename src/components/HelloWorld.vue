1546640199db4fe323e1f2ca191ef1a23fb1d82e
郑刘利 <767848898@qq.com>
2021-02-03 18:28:55 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div
+      ref="aaa"
+      :style="{
+        height: `${containerHeight}px`,
+        overflow: 'auto',
+        boxSizing: 'border-box'
+      }"
+      @scroll="onScroll"
+    >
+      <div
+        :style="{
+          paddingTop: `${startOffset}px`,
+          paddingBottom: `${endOffset}px`
+        }"
+      >
+        <div
+          v-for="(content, i) in renderList"
+          :key="i"
+          :style="{ height: `${itemHeight}px` }"
+        >
+          {{ content }}
+        </div>
+      </div>
+    </div>
+    {{}}
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  xStart = 0;
+  containerHeight = 200;
+  itemHeight = 100;
+  dataSize = 1000;
+  bufferSize = 1;
+
+  list = new Array(1000).fill(0).map((_, i) => `this is content ${i}`);
+
+  mounted() {
+    console.log();
+  }
+
+  get contentHeight() {
+    return this.dataSize * this.itemHeight;
+  }
+
+  get startIndex() {
+    return Math.max(0, this.xStart - Math.ceil(this.viewSize * 0.5));
+  }
+
+  get endIndex() {
+    return Math.min(
+      this.dataSize,
+      this.startIndex + this.viewSize + Math.ceil(this.viewSize * 1.5)
+    );
+  }
+
+  get renderList() {
+    return this.list.slice(this.startIndex, this.endIndex);
+  }
+
+  get viewSize() {
+    return this.containerHeight / this.itemHeight;
+  }
+
+  get startOffset() {
+    return this.startIndex * this.itemHeight;
+  }
+
+  get endOffset() {
+    return this.contentHeight - this.endIndex * this.itemHeight;
+  }
+
+  onScroll(event: ScrollEvent) {
+    const currentIdx = Math.floor(event.target.scrollTop / this.itemHeight);
+    this.xStart = currentIdx;
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 h3 {
   margin: 40px 0 0;
 }
